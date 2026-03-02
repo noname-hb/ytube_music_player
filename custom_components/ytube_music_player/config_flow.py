fe60f35efff6a094530f222b6aa649adbf8719ba
Kolja Windeler <Kolja.Windeler@gmail.com>
2026-03-02 07:44:22 +0100

    comply to HA rules to move URL into code

--- custom_components/ytube_music_player/config_flow.py
+++ custom_components/ytube_music_player/config_flow.py
@@ -142,7 +142,7 @@ async def async_common_step_oauth(self, user_input=None, option_flow = False):  
 			if not(user_input[CONF_RENEW_OAUTH]):
 				# Set CONF_HEADER_PATH before jumping to finish screen (normally set in oauth2 step)
 				self.data[CONF_HEADER_PATH] = os.path.join(self.hass.config.path(STORAGE_DIR),DEFAULT_HEADER_FILENAME+self.data[CONF_NAME].replace(' ','_')+'.json')
-				return self.async_show_form(step_id="finish", data_schema=vol.Schema(await async_create_form(self.hass,self.data,3, option_flow)), errors=self._errors)
+				return self.async_show_form(step_id="finish", data_schema=vol.Schema(await async_create_form(self.hass,self.data,3, option_flow)), errors=self._errors, description_placeholders={"github_url": "https://github.com/KoljaWindeler/ytube_music_player", "google_console_url": "https://console.cloud.google.com/apis/credentials" })
 			
 	return self.async_show_form(step_id="oauth2", data_schema=vol.Schema(await async_create_form(self.hass,user_input,1, option_flow)), errors=self._errors)
 
@@ -185,7 +185,7 @@ async def async_common_step_oauth3(self, user_input=None, option_flow = False): 
 			user_input = self.data
 			return self.async_show_form(step_id="oauth3", data_schema=vol.Schema(await async_create_form(self.hass,self.data,2, option_flow)), errors=self._errors)
 #		OAUTH	
-	return self.async_show_form(step_id="finish", data_schema=vol.Schema(await async_create_form(self.hass,self.data,3, option_flow)), errors=self._errors)
+	return self.async_show_form(step_id="finish", data_schema=vol.Schema(await async_create_form(self.hass,self.data,3, option_flow)), errors=self._errors, description_placeholders={"github_url": "https://github.com/KoljaWindeler/ytube_music_player", "google_console_url": "https://console.cloud.google.com/apis/credentials" })
 
 
 async def async_common_step_finish(self,user_input=None, option_flow = False):
